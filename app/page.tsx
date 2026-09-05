7c94a1d2ded760214686468084964bc7037d0492
Madhav Mishra <madhavmishra083@gmail.com>
2026-09-06 00:53:34 +0530

    till now i haven't done proper routing but I will start when overall all the pages are completed then I will connect it at the last

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
-import { Route } from "next";
+import Login from "./authentication/login"
+import SignUp from "./authentication/signup"
+import ForgotPassword from "./authentication/forgot-password";
+import './globals.css'
+/*import NavBar from "./components/Navbar"*/
 export default function Home() {
   return (
-    <div>
-      
-    </div>
+    <>
+      /*<ForgotPassword />*/
+    </>
   );
 }
