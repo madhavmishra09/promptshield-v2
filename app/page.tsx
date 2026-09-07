352184c112d4e82281610bfa92d4318df3ac1b17
Madhav Mishra <madhavmishra083@gmail.com>
2026-09-07 13:50:00 +0530

    testing it by importing components and authentication pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,21 @@
-import Image from "next/image";
-import Login from "./authentication/login"
-import SignUp from "./authentication/signup"
+import Login from "./authentication/login";
+import SignUp from "./authentication/signup";
 import ForgotPassword from "./authentication/forgot-password";
+import VerifyOTP from "./authentication/verify-otp";
+import ResetPassword from "./authentication/reset-password";
+import ResetSuccess from "./authentication/reset-success";
+import NavBar from "./components/Navbar";
+import PromptInput from "./components/PromptInput";
+import ChatMessage from "./components/ChatMessage";
+import SecurityBadge from "./components/SecurityBadge";
+import MetricCard from "./components/MetricCard"
+import AttackChart from "./components/AttackChart";
+import SecurityEventTable from "./components/SecurityEventTable";
 import './globals.css'
-/*import NavBar from "./components/Navbar"*/
 export default function Home() {
   return (
     <>
-      /*<ForgotPassword />*/
+        <SecurityEventTable/>
     </>
   );
 }
