cd46f635b2d5ec0f2813b64902c9291fdaeb9255
Madhav Mishra <madhavmishra083@gmail.com>
2026-09-18 18:19:36 +0530

    removed dotenv

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,8 @@
 import express from "express"; 
 import type { Application, Request, Response } from "express";
-import dotenv from "dotenv";
+import env from "./config/env.js"
 const app: Application = express();
-dotenv.config();
-const port = Number(process.env.PORT) || 5000;
+const port = env.port;
 console.log(`Port running at ${port}`);
 app.post('/api/health', (req: Request, res: Response): void => {
     res.status(200).json({
