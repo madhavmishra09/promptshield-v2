f2e5f28d6fad2a72f364f1eca3ab75fd1a293af2
Madhav Mishra <madhavmishra083@gmail.com>
2026-09-21 11:03:54 +0530

    Using authentication services

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,16 +1,27 @@
-import express from "express"; 
+import express from "express";
 import type { Application, Request, Response } from "express";
-import env from "./config/env.js"
+
+import env from "./config/env.js";
+import authRoutes from "./routes/auth.routes.js";
+import errorMiddleware from "./middleware/error.middleware.js";
 const app: Application = express();
+
 const port = env.port;
-console.log(`Port running at ${port}`);
-app.post('/api/health', (req: Request, res: Response): void => {
+
+// Middleware
+app.use(express.json());
+
+// Health check
+app.get("/api/health", (req: Request, res: Response): void => {
     res.status(200).json({
         message: "Backend is running",
         status: "ok",
     });
-    return;
 });
-app.listen(port,()=>{
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use(errorMiddleware);
+app.listen(port, () => {
     console.log(`Backend is running at ${port}`);
 });
